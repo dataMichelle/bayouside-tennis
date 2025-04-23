ca6f721739ddd1d19b75585692ca976d15ae682e
Michelle <116389047+dataMichelle@users.noreply.github.com>
2025-04-23 06:55:32 -0500

    fixed slow loading links

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,7 @@
 "use client";
 import StaticNav from "./StaticNav";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 // Load UserNav client-only
 const UserNav = dynamic(() => import("./UserNav"), { ssr: false });
@@ -11,7 +12,11 @@ export default function Navbar() {
     <header className="relative z-[100]">
       <div className="fixed top-4 left-1/2 -translate-x-1/2 h-[3.5rem] w-auto max-w-4xl px-6 rounded-full border border-swamp-400 border-opacity-40 bg-taupe-200 bg-opacity-80 shadow-[0px_8px_16px_rgba(34,85,34,1)] backdrop-blur-[0.5rem] flex items-center gap-4 font-nunito animate-jiggle">
         <StaticNav />
-        <UserNav />
+        <Suspense
+          fallback={<div classname="text-sm text-gray-500">Loading...</div>}
+        >
+          <UserNav />
+        </Suspense>
       </div>
     </header>
   );
