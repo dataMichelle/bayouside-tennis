11d361ab5e0dc1ffdd657cd4e5f560929c976162
Michelle <116389047+dataMichelle@users.noreply.github.com>
2025-04-25 09:15:20 -0500

    fixed connection

--- app/lib/db/users.js
+++ app/lib/db/users.js
@@ -1,20 +1,17 @@
-import clientPromise from "../mongodb";
+import { connectDB } from "../mongodb";
 import { ObjectId } from "mongodb";
 
 export async function getUserByFirebaseUid(firebaseUid) {
-  const client = await clientPromise;
-  const db = client.db("bayou-side-tennis");
+  const db = await connectDB();
   return await db.collection("users").findOne({ firebaseUid });
 }
 
 export async function getUserById(userId) {
-  const client = await clientPromise;
-  const db = client.db("bayou-side-tennis");
+  const db = await connectDB();
   return await db.collection("users").findOne({ _id: new ObjectId(userId) });
 }
 
 export async function getAllUsersByRole(role) {
-  const client = await clientPromise;
-  const db = client.db("bayou-side-tennis");
+  const db = await connectDB();
   return await db.collection("users").find({ role }).toArray();
 }
